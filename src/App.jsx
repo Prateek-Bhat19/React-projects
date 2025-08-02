b555833665c3924dc85e66c9c7ed0ec234e66056
Prateek <prateekbhat1919@gmail.com>
2025-08-02 20:37:15 +0530

    star-rating

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Accordian from './Components/accordian'
-import RandomColor from './Components/random-color'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Accordian from "./Components/accordian";
+import RandomColor from "./Components/random-color";
+import StarRating from "./Components/star-rating";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-   <div>
-    {/* accordian component */}
-    <Accordian />
-    {/* Random color generatr */}
-  <RandomColor />
-   </div>
-  )
+    <div>
+      {/* accordian component */}
+      {/* <Accordian /> */}
+      {/* Random color generatr */}
+      {/* <RandomColor /> */}
+      {/* Star-rating */}
+      <StarRating noOfStars={10}/>
+    </div>
+  );
 }
 
-export default App
+export default App;
